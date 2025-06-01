ed3ebf328e14e5c31c8cbfee46e5e7eed994c068
hoanganh5350 <hoanganh2000vip1@example.com>
2025-06-01 21:20:30 +0700

    Completed Get Inspried in tab Services

--- front-end/src/features/ServicesView/GetInspried/CreateCaptionFromIdea.tsx
+++ front-end/src/features/ServicesView/GetInspried/CreateCaptionFromIdea.tsx
@@ -1,38 +1,72 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import "../Services.scss";
 import ContentItem from "../../../components/ContentItem/ContentItem";
+import { post } from "../../../api/http";
 
 interface CreateCaptionFromIdeaProps {
   ideaGeneratedFromAI: string;
+  topic: string;
 }
 
-const listCaptionsGeneratedFake = [
-  {
-    title: "Skipli AI - the smarter",
-    content:
-      "Introducing Skipli AI - the smarter, faster way to craft compelling content  Experience all the magic of Al-driven wriing assistant and get great results with fewer headaches.#AI #ContentMarketing #Content",
-  },
-  {
-    title: "SkipliAl creating content Social Media",
-    content:
-      "Say goodbye to writer's block  #SkipliAl is now available to make creating attention-grabbing content easie thaneverGeready totake yoursocialmedia game to the next level with #AI#SocialMedia #Writing",
-  },
-];
+// const listCaptionsGeneratedFake = [
+//   {
+//     title: "Skipli AI - the smarter",
+//     content:
+//       "Introducing Skipli AI - the smarter, faster way to craft compelling content  Experience all the magic of Al-driven wriing assistant and get great results with fewer headaches.#AI #ContentMarketing #Content",
+//   },
+//   {
+//     title: "SkipliAl creating content Social Media",
+//     content:
+//       "Say goodbye to writer's block  #SkipliAl is now available to make creating attention-grabbing content easie thaneverGeready totake yoursocialmedia game to the next level with #AI#SocialMedia #Writing",
+//   },
+// ];
 
 const CreateCaptionFromIdea = (props: CreateCaptionFromIdeaProps) => {
-  const { ideaGeneratedFromAI } = props;
+  const { ideaGeneratedFromAI, topic } = props;
+  const [loading, setLoading] = useState<boolean>(false);
   const [listCaptionsGenerated, setListCaptionsGenerated] = useState<any[]>([]);
+  const [loadingItem, setLoadingItem] = useState<boolean>(false);
 
-  const handelGenerateCaption = () => {
-    setListCaptionsGenerated(listCaptionsGeneratedFake);
+  const handelGenerateCaption = async () => {
+    setLoading(true);
+    try {
+      const response = await post<string[]>("/content/captions-from-idea", {
+        idea: ideaGeneratedFromAI,
+      });
+      if (response && Array.isArray(response) && response.length > 0) {
+        setListCaptionsGenerated(
+          response.map((item: string) => ({ content: item }))
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   const handleShareCaptions = (data: any) => {
     console.log(data);
   };
 
-  const handleSaveCaptions = (data: any) => {
-    console.log(data);
+  const handleSaveCaptions = async (data: { content: string }) => {
+    setLoadingItem(true);
+    try {
+      const response = await post<{ success: boolean }>("/content/save", {
+        topic,
+        data: [`${data.content}`],
+        phoneNumber: localStorage.getItem("phone"),
+      });
+      if (response && response.success) {
+        setListCaptionsGenerated((prev) => {
+          return prev?.filter((item) => item.content !== data.content);
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoadingItem(false);
+    }
   };
 
   return (
@@ -43,8 +77,9 @@ const CreateCaptionFromIdea = (props: CreateCaptionFromIdeaProps) => {
         <button
           className="ButtonGenerateCaption"
           onClick={handelGenerateCaption}
+          disabled={loading}
         >
-          Generate caption
+          {loading ? "Generating caption..." : "Generate caption"}
         </button>
       </div>
       {listCaptionsGenerated && listCaptionsGenerated.length > 0 && (
@@ -59,6 +94,7 @@ const CreateCaptionFromIdea = (props: CreateCaptionFromIdeaProps) => {
               content={el.content}
               onShare={() => handleShareCaptions(el)}
               onSave={() => handleSaveCaptions(el)}
+              saving={loadingItem}
             />
           ))}
         </div>
